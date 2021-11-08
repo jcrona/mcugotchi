0e7c7e0087e9b6a1553a54a8b9adb164ba0be938
Jean-Christophe Rona <jc@rona.fr>
2021-11-08 18:35:45 +0100

    Storage offsets and size are MCU dependent

--- src/mcu/inc/storage.h
+++ src/mcu/inc/storage.h
@@ -22,16 +22,7 @@
 
 #include <stdint.h>
 
-#define STORAGE_BASE_ADDRESS				0x800D000
-
-#define STORAGE_SIZE					0x13000
-#define STORAGE_PAGE_SIZE				512 // 2KB in words (sizeof(uint32_t))
-
-#define STORAGE_ROM_OFFSET				0x0
-#define STORAGE_ROM_SIZE				0xC00 // 12KB in words (sizeof(uint32_t))
-
-#define STORAGE_FS_OFFSET				0xC00
-#define STORAGE_FS_SIZE					0x4000 // 64KB in words (sizeof(uint32_t))
+#include "mcu.h"
 
 
 int8_t storage_read(uint32_t offset, uint32_t *data, uint32_t length);
--- src/mcu/stm32/STM32F0/mcu.h
+++ src/mcu/stm32/STM32F0/mcu.h
@@ -24,4 +24,16 @@
 #define MCU_TIME_FREQ_NUM					1
 #define MCU_TIME_FREQ_DEN					128
 
+/* Storage related offsets and sizes */
+#define STORAGE_BASE_ADDRESS					0x800D000
+
+#define STORAGE_SIZE						0x13000
+#define STORAGE_PAGE_SIZE					512 // 2KB in words (sizeof(uint32_t))
+
+#define STORAGE_ROM_OFFSET					0x0
+#define STORAGE_ROM_SIZE					0xC00 // 12KB in words (sizeof(uint32_t))
+
+#define STORAGE_FS_OFFSET					0xC00
+#define STORAGE_FS_SIZE						0x4000 // 64KB in words (sizeof(uint32_t))
+
 #endif /* _MCU_H_ */
